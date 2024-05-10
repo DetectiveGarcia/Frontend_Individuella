d8d232b09eed172c4d6f2454918a573fb81b19e0
DetectiveGarcia <144781010+DetectiveGarcia@users.noreply.github.com>
2024-05-10 19:33:29 +0200

    Added alert after deleting comment

--- src/Components/CommentSection.jsx
+++ src/Components/CommentSection.jsx
@@ -10,6 +10,7 @@ const CommentSection = ({ comment, baseURL }) => {
     try {
       const response = await axios.delete(`${baseURL}/comment/delete/${comment._id}`)
       if(response.status === 200){
+        alert('Comment deleted')
         console.log('Comment succefully deleted');
         window.location.reload()
       }
